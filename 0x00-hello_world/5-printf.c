8b8b9c8d9ee724d2ebbf67504a619cb345f8bd07
Isaiah Woko <wokoisaiah68@gmail.com>
2023-02-16 16:18:30 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ * Description: use the function printf to print a string
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
