2e42c23a8c88a4c4b5c43469a0d4b4a4918573ff
Isaiah Woko <wokoisaiah68@gmail.com>
2023-02-17 15:49:33 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char lower
+
+	for (lower 'z'; lower >= 'a'; lower--)
+		putchar(lower);
+
+	putchar('\n');
+
+	return (0);
+}
