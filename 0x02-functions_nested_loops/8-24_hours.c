c69b70b256adf2728c30cbb298a6e9b4c8aebc91
Isaiah Woko <wokoisaiah68@gmail.com>
2023-02-22 08:52:38 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
 /**
- * Jack_ bauer - entry point
+ * jack_bauer - prints every minute of the day of Jack Bauer
  *
- * Description: prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  *
  */
+
 void jack_bauer(void)
 {
 	int i, j;
