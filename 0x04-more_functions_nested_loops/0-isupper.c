0b0bad03c020c0222256831eb32cec2f158ec1c2
Isaiah Woko <wokoisaiah68@gmail.com>
2023-02-23 11:56:18 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  _isupper - function that checks for uppercase character
+ *  @c : character to be checked
+ *  Return: 0 or 1
+ *
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c<= 90);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project folder consists of all the project tasks that would be carried out in the 0x04. C - More functions, more nested loops
