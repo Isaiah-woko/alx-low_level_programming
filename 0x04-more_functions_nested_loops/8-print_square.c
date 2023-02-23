1f56b31c00c36d7894fa036a66955f895a33c956
Isaiah Woko <wokoisaiah68@gmail.com>
2023-02-23 15:32:11 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,20 +10,19 @@ void print_square(int size)
 {
 	int i = 0, ii;
 
-	while (i < size && size > 0)
+	 while (i < size && size > 0)
 	{
+		ii = 0;
+		while (ii < size)
+		{
+			_putchar('#');
+			ii++;
+		}
 
-	ii = 0;
-	while (ii < size)
-	{
-		_putchar('#');
-		_putchar(' ');
-		ii++;
-	}
 		_putchar('\n');
 		i++;
 	}
-	if (size == 0)
+	if (i == 0)
 		_putchar('\n');
 
 }
