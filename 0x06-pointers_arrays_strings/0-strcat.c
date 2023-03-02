3db4dce06cda87107cc7bdce485729667d50007c
Isaiah Woko <wokoisaiah68@gmail.com>
2023-03-02 07:40:54 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer direction
+ * @src: pointer source
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i =  0;
+	int j = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	do {
+		dest[i] = src[j];
+		i++;
+		j++;
+	} while (src[j - 1] != '\0');
+
+	return (dest);
+} 
