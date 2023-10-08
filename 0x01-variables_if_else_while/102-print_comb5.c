8b267bafb0a8fbb7a11064e5cd9e84515e7037d2
Isaiah woko <wokoisaiah68@gmail.com>
2023-10-08 14:35:01 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: a program that prints two two-digit numbers.
+ * Return: 0
+ */
+
 int main(void)
 {
-	int i, j;
-	for (i = 0; i <= 99; i++)
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (b = 0; b <= 9; b++)
 		{
-			
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (((c + d) > (a + b) && c >= a) || a < c)
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(' ');
+						putchar(c + '0');
+						putchar(d + '0');
+
+					if (a != 9 || b != 8 || c != 9 || d != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
 		}
 	}
+	putchar('\n');
+	return (0);
 }
