75588588679992586dc0ca67873721705b03b63a
Isaiah woko <wokoisaiah68@gmail.com>
2023-10-11 14:07:14 -0700

    10-add.c && 9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  add - unction that adds two integers and returns the result
+ * @a: number to be added
+ * @b: number to be added
+ * Return: the result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
