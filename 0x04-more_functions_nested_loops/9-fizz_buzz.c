2cc3a1c0874b7268e3d736edbe2a8334843bca08
Isaiah woko <wokoisaiah68@gmail.com>
2023-10-12 19:27:52 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,6 @@ int main(void)
 {
 	int i = 1;
 
-	//for (i = 1; i <= 100; i++)
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
@@ -34,9 +33,10 @@ int main(void)
 		{
 			putchar(' ');
 		}
+
 		i++;
 	}
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }
