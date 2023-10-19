1e05ed9696ebf9d098a56ca350c2447f4c1c0712
Isaiah woko <wokoisaiah68@gmail.com>
2023-10-19 02:07:59 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -23,12 +23,7 @@ int _strcmp(char *s1, char *s2)
 
 	if (s1[i] == '\0' && s2[i] == '\0')
 		return (0);
-
-
-
-	if (s1[i] == '\0')
-		return (-15);
 	else
-		return (15);
+		return (*s1 - *s2);
 
 }
