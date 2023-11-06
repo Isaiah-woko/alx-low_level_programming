681b57ad4953464daead0be7097eda2de1c59a3e
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-06 06:59:54 -0800

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function that initialize a variable of type
+ * @d: pointer to the structure of type struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of owner of the dog
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
