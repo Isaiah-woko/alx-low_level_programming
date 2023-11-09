a8181226ba657344776a9e2cd6f02dc5a08ffb5e
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-08 16:54:02 -0800

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,11 +20,11 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0')
+	/**if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
-	}
+	}*/
 	s = argv[2][0];
 	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
 	{
