3a056544cde3706179bdc4874662db1006cb5914
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-18 11:56:08 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_before_main - a function that executes before the main function
+ *
+ * Return: void
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
