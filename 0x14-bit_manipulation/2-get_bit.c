fcb0bacf40f52dd32f534e45e68d1222bde662bb
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-23 08:55:03 -0800

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - a function that returns the value
+ * of a bit at a given index.
+ *
+ * @n: number to get bit
+ * @index: the index to shift to
+ * Return: index on success or -1 on failure
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int shiftBit;
+
+	if (n == '\0')
+	{
+		return (-1);
+	}
+
+	shiftBit = (n >> index) & 1;
+
+	return (shiftBit);
+}
