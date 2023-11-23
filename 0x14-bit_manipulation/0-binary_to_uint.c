8a1fd54e58d4705b699ff9187574f432b41286b2
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-23 07:36:42 -0800

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+int _strlen(const char *str);
+
+
+/**
+ * _strlen - check length of string
+ * @str: string to check
+ * Return: length of string
+ */
+
+int _strlen(const char *str)
+{
+	int len  = 0;
+
+	while (str[len] != 0)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * binary_to_uint -  a function that converts a binary
+ * number to an unsigned int.
+ * @b: a poiner to the string
+ * Return: the converted number or 0 if failed
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int len = _strlen(b);
+	int base = 1, i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+		{
+			decimal += base;
+		}
+		else if (b[i] != '0' || b == NULL)
+		{
+			return (0);
+		}
+		base *= 2;
+	}
+
+	return (decimal);
+}
