a136fc64a8a063285905c3a8cd1b90acc20fe11a
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-23 09:27:01 -0800

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * set_bit - a function that returns the value
+ * of a bit at a given index.
+ *
+ * @n: number to get bit
+ * @index: the index to shift to
+ * Return: index on success or -1 on failure
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	*n |= (1UL << index);
+
+	return (1);
+}
