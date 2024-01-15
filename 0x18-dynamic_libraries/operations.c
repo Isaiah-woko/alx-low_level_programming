1e7568fd9d86f3635f2c39b43df5e9d59faa8a63
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-16 00:41:36 +0100

    100-operations.so

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * add - function to add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function to subtract two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - function to multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * div - function to divide two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - function to mod two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
